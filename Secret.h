6f721691a55a15f35c8ad0cdf7b74704687e0e5f
CHs-project-max <laychhayhout010@gmail.com>
2025-05-26 23:48:45 +1000

    Add files via upload

---
+++ Secret.h
@@ -0,0 +1,13 @@
+#define WIFI_SSID "Telstra1AA87A"
+#define WIFI_PASSWORD "rqp2vsnk44b7unk4"
+#define MQTT_SERVER "7a753cf62ab34b578ab71f6952b72df2.s1.eu.hivemq.cloud"
+#define MQTT_PORT 8883
+#define MQTT_SUBTOPIC "RPi"
+#define MQTT_USER "hivemq.webclient.1747458456910"
+#define MQTT_PASSWORD "$%0?&983TJGuKYfmxzbL"
+
+#define openClose_Time 2000 //replace with your own desire this is open and close time so try it out so the motor won't over spin
+
+uint32_t IR_opensignal = 3175222798;
+uint32_t IR_closesignal = 3158511118;
+uint32_t IR_Manualsignal = 3108376078;
